40296725ce9cae2e05c067a912e65ab7db50ed90
DiegoB <diegobaesv@gmail.com>
2025-03-27 22:04:39 -0500

    subcateeogiras view

---
+++ app/Models/Subcategoria.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+use App\Models\Categoria;
+use Illuminate\Database\Eloquent\Model;
+
+class Subcategoria extends Model
+{
+    protected $table = 'subcategorias';
+
+    protected $fillable = [
+        'nombre',
+        'imagen_url',
+        'id_categoria'
+    ];
+
+    public function categoria(){
+        return $this->belongsTo(Categoria::class,'id_categoria');
+    }
+
+    public $timestamps = false;
+}
--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,9 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CategoriaController;
+use App\Http\Controllers\SubcategoriaController;
 
 Route::get('/', function () { return view('welcome'); });
 
 Route::get('/categorias', [CategoriaController::class,'index'])->name('categorias.index');
+Route::get('/categorias/{idCategoria}/subcategorias',[SubcategoriaController::class,'index'])->name('subcategorias.index');
